9463e43e155f62d2cf68815d734cda42a6aff73d
Sakshi Gadhave <118893317+sakshigadhave78@users.noreply.github.com>
2022-12-12 11:47:09 +0530

    Add files via upload

---
+++ React/Restaurant/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+ import Resturant from "./components/Basic/Restuarant";
+// import UseState from "./component/Hooks/useState";
+//import UseEffect from "./components/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return(
+    <>
+    <div className="Card">
+        <Resturant/>
+    </div>
+    </>
+  );
+};
+
+export default App;
